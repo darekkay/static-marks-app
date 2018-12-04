26015cd25fd4d64fd8f500cdb33638c6d9d6b05c
Darek Kay <darekkay@gmail.com>
2018-12-04 17:34:30 +0100

    Include app version in the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,19 +1,16 @@
 import React from "react";
 
-import Icon from "../icon/Icon";
 import Link from "../link/Link";
 import "./Footer.css";
 
 const Footer = () => (
   <footer>
     <Link url="https://darekkay.com">Handmade with &hearts; by Darek Kay</Link>
-
-    <div className="github">
+    {process.env.REACT_APP_VERSION && (
       <Link url="https://github.com/darekkay/static-marks">
-        Fork me on GitHub
+        Version {process.env.REACT_APP_VERSION}
       </Link>
-      <Icon name="github" position="right" />
-    </div>
+    )}
   </footer>
 );
 
