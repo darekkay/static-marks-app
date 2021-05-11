75473219821e66caba6f0e36368ba0cd6ec8bb1f
Darek Kay <hello@darekkay.com>
2021-05-11 09:55:43 +0200

    Extracts Collections component

--- src/App.js
+++ src/App.js
@@ -6,36 +6,37 @@ import Footer from "./components/footer/Footer";
 import Collection from "./components/collection/Collection";
 import "./App.scss";
 
-const App = ({ title, projectKeys, projects, initialFilter }) => {
+const renderTitle = (projectKey, title) => {
+  const prefix = projectKey === undefined ? "" : `${projectKey} | `;
+  document.title = `${prefix}${title}`;
+};
+
+const Collections = ({ projects, projectKey, title, currentFilter }) => {
+  renderTitle(projectKey, title);
+  return (
+    <div className="collections" aria-live="polite">
+      {projects
+        .filter(
+          (project) => projectKey === undefined || projectKey === project.key
+        )
+        .map((project) =>
+          project.collections.map((collection) => (
+            <Collection
+              key={collection.title}
+              title={collection.title}
+              source={project.key}
+              buckets={collection.buckets}
+              currentFilter={currentFilter}
+            />
+          ))
+        )}
+    </div>
+  );
+};
+
+const App = (props) => {
+  const { title, projectKeys, projects, initialFilter } = props;
   const [currentFilter, applyFilter] = useState(initialFilter);
-
-  const renderTitle = (key) => {
-    const prefix = key === undefined ? "" : `${key} | `;
-    document.title = `${prefix}${title}`;
-  };
-
-  // TODO: improve
-  // eslint-disable-next-line react/display-name
-  const renderCollections = (key) => () => {
-    renderTitle(key);
-    return (
-      <div className="collections" aria-live="polite">
-        {projects
-          .filter((set) => key === undefined || set.key === key)
-          .map((set) =>
-            set.collections.map((collection) => (
-              <Collection
-                key={collection.title}
-                title={collection.title}
-                source={set.key}
-                buckets={collection.buckets}
-                currentFilter={currentFilter}
-              />
-            ))
-          )}
-      </div>
-    );
-  };
 
   return (
     <div className="app">
@@ -48,18 +49,17 @@ const App = ({ title, projectKeys, projects, initialFilter }) => {
         <h1 className="sr-only">{title}</h1>
         <div className="scrollable-y">
           <Switch>
-            <Route exact path="/" render={renderCollections()} />
-            {projects.map((set) => (
-              <Route
-                key={set.key}
-                exact
-                path={`/${set.key}`}
-                render={
-                  currentFilter
-                    ? renderCollections()
-                    : renderCollections(set.key)
-                }
-              />
+            <Route exact path="/">
+              <Collections currentFilter={currentFilter} {...props} />
+            </Route>
+            {projects.map((project) => (
+              <Route key={project.key} exact path={`/${project.key}`}>
+                <Collections
+                  currentFilter={currentFilter}
+                  projectKey={currentFilter ? undefined : project.key}
+                  {...props}
+                />
+              </Route>
             ))}
           </Switch>
           <div className="no-results">
