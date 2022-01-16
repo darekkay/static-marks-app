613679f0c18bd4f4e199363bc5263bf4888f5025
Nikesh <nikesh123u@gmail.com>
2022-01-16 21:37:56 +0545

    Import Mock method

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./matchMedia"; //Mocking methods which are not implemented in JSDOM
 import { HashRouter } from "react-router-dom";
 
 import App from "./App";
