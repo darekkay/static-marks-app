2f9e5503c4c2402e82e0891ee4bc9f3831722dab
Darek Kay <hello@darekkay.com>
2022-01-22 11:11:00 +0100

    Add dark mode (#5)
    Co-authored-by: Nikesh Shrestha <63302392+devnikesh@users.noreply.github.com>

---
+++ src/setupTests.js
@@ -0,0 +1,11 @@
+// matchMedia is not implemented in JSDOM
+window.matchMedia = (query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addListener: jest.fn(), // Deprecated
+  removeListener: jest.fn(), // Deprecated
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  dispatchEvent: jest.fn(),
+});
