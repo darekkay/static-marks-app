748563b6cbae9b7335facfe0ef62f4993a364782
lando <du33169@qq.com>
2024-07-08 20:15:38 +0800

    Add Light/Dark Theme Toggle Button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,36 +1,55 @@
 import React from "react";
 
+import Button from "../button/Button";
 import Menu from "../menu/Menu";
 import Filter from "../filter/Filter";
 
 import "./Header.scss";
 
+
 const Header = ({ projectKeys, applyFilter, currentFilter }) => {
   const clearFilterOnProjectChange = () => applyFilter("");
+  const toggleTheme = () => {
+    const currentTheme = document.documentElement.dataset.theme;
+    if (currentTheme === "default") {
+      document.documentElement.dataset.theme = "dark";
+    }
+    else if (currentTheme === "dark") {
+      document.documentElement.dataset.theme = "default";
+    }
+    localStorage.setItem("theme", document.documentElement.dataset.theme);
+  }
   return (
     <header>
       <div className="header-inner">
         <Filter applyFilter={applyFilter} currentValue={currentFilter} />
-
-        {projectKeys.length > 1 && (
-          // Display project selection only for more than 1 projects
-          <Menu
-            items={[
-              {
-                route: "/",
-                title: "all",
-                icon: "clone",
-                onClick: clearFilterOnProjectChange,
-              },
-              ...projectKeys.map((projectKey) => ({
-                route: `/${projectKey}`,
-                projectKey,
-                icon: "folder",
-                onClick: clearFilterOnProjectChange,
-              })),
-            ]}
+        <div className="header-actions">
+          {projectKeys.length > 1 && (
+            // Display project selection only for more than 1 projects
+            <Menu
+              items={[
+                {
+                  route: "/",
+                  title: "all",
+                  icon: "clone",
+                  onClick: clearFilterOnProjectChange,
+                },
+                ...projectKeys.map((projectKey) => ({
+                  route: `/${projectKey}`,
+                  projectKey,
+                  icon: "folder",
+                  onClick: clearFilterOnProjectChange,
+                })),
+              ]}
+            />
+          )}
+          <Button
+            className="toggle-theme-btn"
+            label="Toggle Light/Dark Mode"
+            icon="circle-half"
+            onClick={toggleTheme}
           />
-        )}
+        </div>
       </div>
     </header>
   );
